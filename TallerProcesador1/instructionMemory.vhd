38ee6632b398222f08daaf6e1bdc31869506bc28
Dasel <daselopez@gmail.com>
2016-04-17 08:41:52 -0500

    implementación de InstructionMemory

--- TallerProcesador1/instructionMemory.vhd
+++ TallerProcesador1/instructionMemory.vhd
@@ -19,6 +19,8 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use std.textio.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -38,7 +40,37 @@ end instructionMemory;
 
 architecture arqIM of instructionMemory is
 
-begin
+	type rom_type is array (0 to 63) of std_logic_vector (31 downto 0);
+		
+		impure function InitRomFromFile (RomFileName : in string) return rom_type is
+			FILE RomFile : text open read_mode is RomFileName;
+			variable RomFileLine : line;
+			variable temp_bv : bit_vector(31 downto 0);
+			variable temp_mem : rom_type;
+			begin
+				for I in rom_type'range loop
+					readline (RomFile, RomFileLine);
+					read(RomFileLine, temp_bv);
+					temp_mem(i) := to_stdlogicvector(temp_bv);
+				end loop;
+			return temp_mem;
+		end function;
+		
+		signal instructions : rom_type := InitRomFromFile("testJMPL.data");
+		
+	begin
+	--reset,address, instructions)
+		process(reset,address, instructions)--clk)
+		begin
+			--if(rising_edge(clk))then
+				if(reset = '1')then
+					outInstruction <= (others=>'0');
+				else
+					outInstruction <= instructions(conv_integer(address(5 downto 0)));
+				end if;
+			--end if;
+		end process;
+
 
 
 end arqIM;
